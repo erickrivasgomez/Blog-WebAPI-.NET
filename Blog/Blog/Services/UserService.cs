03aebfc3dd6ed38cfe66ada7b2f9ab3aaae45d6f
Erick Rivas Gómez <rivaserick@outlook.com>
2020-02-17 09:45:52 -0600

    Created users services

---
+++ Blog/Blog/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Blog.Models;
+using Blog.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Services
+{
+    public class UserService
+    {
+        private UserRepository _userRepository;
+
+        public UserService(UserRepository blogRepository)
+        {
+            _userRepository = blogRepository;
+        }
+
+        public DTO GetUsers()
+        {
+            return _userRepository.GetUsers();
+        }
+    }
+}
