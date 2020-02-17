115bdaca8ff9b6f0a2f43978e8d9b436b214df99
Erick Rivas Gómez <rivaserick@outlook.com>
2020-02-17 17:42:35 -0600

    Finished posts repository

--- Blog/Blog/Repositories/PostRepository.cs
+++ Blog/Blog/Repositories/PostRepository.cs
@@ -1,11 +1,12 @@
 ﻿using Blog.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Blog.Repositories
 {
     public class PostRepository
     {
-        public List<Model> Posts = new List<Model>()
+        public List<Post> Posts = new List<Post>()
         {
             new Post{ Id = 1, Title = "Post1", Content = "Post 1 content", PublishedAt = "01/01/2020", UserId = 1 },
             new Post{ Id = 2, Title = "Post2", Content = "Post 2 content", PublishedAt = "02/01/2020", UserId = 2 },
@@ -14,9 +15,78 @@ namespace Blog.Repositories
             new Post{ Id = 5, Title = "Post5", Content = "Post 5 content", PublishedAt = "05/01/2020", UserId = 5 },
         };
 
-        public DTO GetPosts()
+        public PostDTO GetPosts()
         {
-            return new DTO(error: false, message: "Posts obtained successfully", content: Posts);
+            return new PostDTO
+                (
+                error: false,
+                messages: new List<string>() { "Post list" },
+                content: Posts
+                );
+        }
+
+        public PostDTO AddPost(Post newPost)
+        {
+            PostDTO response = new PostDTO();
+
+            Post post = Posts.FirstOrDefault(u => u.Id == newPost.Id);
+
+            if (post == null)
+            {
+                Posts.Add(newPost);
+                response.Error = false;
+                response.Messages = new List<string>() { "Post added successfully!" };
+            }
+            else
+            {
+                response.Error = true;
+                response.Messages = new List<string>() { "Error: Id already in use." };
+            }
+
+            return response;
+        }
+
+        public PostDTO EditPost(int id, Post newPost)
+        {
+            PostDTO response = new PostDTO();
+
+            Post post = Posts.FirstOrDefault(u => u.Id == id);
+
+            if (post != null)
+            {
+                Posts.Remove(post);
+                Posts.Add(newPost);
+                response.Error = false;
+                response.Messages = new List<string>() { "Post updated successfully!" };
+            }
+            else
+            {
+                response.Error = true;
+                response.Messages = new List<string>() { "Error: post not found" };
+            }
+
+            return response;
+        }
+
+        public PostDTO DeletePost(int id)
+        {
+            PostDTO response = new PostDTO();
+
+            Post post = Posts.FirstOrDefault(u => u.Id == id);
+
+            if (post != null)
+            {
+                Posts.Remove(post);
+                response.Error = false;
+                response.Messages = new List<string>() { "Post deleted successfully!" };
+            }
+            else
+            {
+                response.Error = true;
+                response.Messages = new List<string>() { "Error: post not found" };
+            }
+
+            return response;
         }
     }
 }
