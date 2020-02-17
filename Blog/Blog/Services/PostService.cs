56d80c7d2f337233d20301dbf8c377970577417b
Erick Rivas Gómez <rivaserick@outlook.com>
2020-02-17 17:45:20 -0600

    Created post service

---
+++ Blog/Blog/Services/PostService.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Models;
+using Blog.Repositories;
+
+namespace Blog.Services
+{
+    public class PostService
+    {
+        private PostRepository _postRepository;
+
+        public PostService(PostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public PostDTO GetPosts()
+        {
+            return _postRepository.GetPosts();
+        }
+
+        public PostDTO AddPost(Post post)
+        {
+            return _postRepository.AddPost(post);
+        }
+
+        public PostDTO EditPost(int id, Post post)
+        {
+            return _postRepository.EditPost(id, post);
+        }
+
+        public PostDTO DeletePost(int id)
+        {
+            return _postRepository.DeletePost(id);
+        }
+    }
+}
