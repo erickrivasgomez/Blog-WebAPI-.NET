a8473f99322e20c512cb996e9bbb5cacbb7efddd
Erick Rivas Gomez <rivaserick@outlook.com>
2020-02-16 23:55:44 -0600

    Models created

---
+++ Blog/Blog/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace Blog.Models
+{
+    public class Post
+    {
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string content { get; set; }
+        
+        public string publishedat { get; set; }
+        
+        public int userid { get; set; }
+    }
+}
