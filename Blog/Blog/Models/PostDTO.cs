8fc6a59db3b8ae2fafb6f8383ae66409792618a2
Erick Rivas Gómez <rivaserick@outlook.com>
2020-02-17 17:42:06 -0600

    Update models

---
+++ Blog/Blog/Models/PostDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class PostDTO
+    {
+        public bool Error { get; set; }
+
+        public IEnumerable<string> Messages { get; set; }
+
+        public IEnumerable<Post> Content { get; set; }
+
+        public PostDTO()
+        {
+            this.Messages = new List<string>();
+            this.Content = new List<Post>();
+        }
+
+        public PostDTO(bool error, List<string> messages, List<Post> content)
+        {
+            this.Error = error;
+            this.Messages = messages;
+            this.Content = content;
+        }
+    }
+}
